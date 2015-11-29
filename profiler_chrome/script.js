c42b5c80875ac8bcbeae09c7dce3a990648c0d6c
Sally Kong <kongsally@users.noreply.github.com>
2015-11-28 23:38:42 -0500

    added runtion sendMessage function

--- profiler_chrome/script.js
+++ profiler_chrome/script.js
@@ -1,11 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
 
   var checkPageButton = document.getElementById('checkPage');
+  // console.log(this.programs);
+  // for (var i = 0; i < programs.length; i++) {
+  //   $("#programs_list").add("<option>"+ this.programs[i] + "</option>");
+  // }
+  console.log("chrome extension getViews");
+  console.log(chrome.extension.getViews());
+
+  chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    console.log(request.program);
+    $("#message").text(request.canvas_message);
+  
+  });
+
   checkPageButton.addEventListener('click', function() {
-    chrome.tabs.executeScript(null, {file: "profiler.js"},
-      function(message){ 
-        $("#message").text(message);
-      } );
+    chrome.tabs.executeScript(null, {file: "profiler.js"});
   }, false);
 
 }, false);
